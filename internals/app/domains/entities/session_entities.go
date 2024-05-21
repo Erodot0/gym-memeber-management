9229fac28f243804d46f63f226a99df33cb5b99a
Erodot0 <janmanpreet.singh@gmail.com>
2024-05-21 21:31:56 +0200

    added cache methods to session

--- internals/app/domains/entities/session_entities.go
+++ internals/app/domains/entities/session_entities.go
@@ -1,6 +1,12 @@
 package entities
 
-import "time"
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/goccy/go-json"
+)
 
 type Session struct {
 	Token     string        `json:"token" gorm:"unique;not null;index"`
@@ -8,4 +14,29 @@ type Session struct {
 	UserAgent string        `json:"user_agent" gorm:"not null;index"`
 	UserID    uint          `json:"user_id" gorm:"not null;index"`
 	Expires   time.Duration `json:"expires" gorm:"not null"`
+}
+
+func (s *Session) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, s)
+}
+
+func (s *Session) SetCacheKey() string {
+	stringId := strconv.FormatUint(uint64(s.UserID), 10)
+	return fmt.Sprintf("session:%s:%s", stringId, s.Token)
+}
+
+func (s *Session) SetCacheExpiration() time.Duration {
+	return s.Expires
+}
+
+func (s *Session) GetCacheKey() string {
+	stringId := "*"
+	token := "*"
+	if s.UserID != 0 {
+		stringId = strconv.FormatUint(uint64(s.UserID), 10)
+	}
+	if s.Token != "" {
+		token = s.Token
+	}
+	return fmt.Sprintf("session:%s:%s", stringId, token)
 }
