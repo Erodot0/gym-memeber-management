d3265eff35b81787e544f452b4e90d302479290b
Erodot0 <janmanpreet.singh@gmail.com>
2024-05-21 19:42:44 +0200

    added user creation handler

--- internals/app/handlers/user_handler.go
+++ internals/app/handlers/user_handler.go
@@ -1,8 +1,6 @@
 package handlers
 
 import (
-	"fmt"
-
 	"github.com/Erodot0/gym-memeber-management/internals/app/domains/entities"
 	"github.com/Erodot0/gym-memeber-management/internals/app/domains/ports"
 	"github.com/gofiber/fiber/v2"
@@ -14,14 +12,31 @@ type UserHandlers struct {
 	User ports.UserServices
 }
 
+// CreateUser handles the creation of a new user.
+//
+// It takes a fiber.Ctx parameter `c` representing the HTTP request context.
+// It returns an error indicating whether the user creation was successful or not.
 func (h *UserHandlers) CreateUser(c *fiber.Ctx) error {
 	user := new(entities.User)
 	if err := h.Parser.ParseData(c, user); err != nil {
 		return h.Http.BadRequest(c, "Error parsing data")
 	}
 
-	fmt.Println(user)
+	//TODO: Validate user
 
+	// Hash password
+	hashedPassword, err := h.User.EcnrypPassword(user.Password)
+	if err != nil {
+		return h.Http.InternalServerError(c, "Error hashing password")
+	}
+	user.Password = hashedPassword
+
+	// Create user
+	if err := h.User.CreateUser(user); err != nil {
+		return h.Http.InternalServerError(c, "Error creating user")
+	}
+
+	
 	user.RemovePassword()
 	return h.Http.Success(c, []interface{}{user}, "User created")
 }
