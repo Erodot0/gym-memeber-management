d4522556a271434a7aba48d2b318c38e2306ecc0
Erodot0 <janmanpreet.singh@gmail.com>
2024-05-21 17:52:02 +0200

    created test handler to create user

---
+++ internals/app/handlers/user_handler.go
@@ -0,0 +1,24 @@
+package handlers
+
+import (
+	"fmt"
+
+	"github.com/Erodot0/gym-memeber-management/internals/app/domains/entities"
+	"github.com/Erodot0/gym-memeber-management/internals/app/domains/ports"
+	"github.com/gofiber/fiber/v2"
+)
+
+type UserHandlers struct {
+	Parser ports.ParserAdapters
+	Http ports.HttpAdapters
+}
+
+func (h *UserHandlers) CreateUser(c *fiber.Ctx) error {
+	user := new(entities.User)
+	if err := h.Parser.ParseData(c, user); err != nil {
+		return h.Http.BadRequest(c, "Error parsing data")
+	}
+
+	fmt.Println(user)
+	return h.Http.Success(c, user, "User created")
+}
