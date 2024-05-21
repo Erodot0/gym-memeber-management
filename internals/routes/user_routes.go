95bd5b6cf25aa040195e0b8e24f911a31adf5918
Erodot0 <janmanpreet.singh@gmail.com>
2024-05-21 17:52:23 +0200

    created test post user route

---
+++ internals/routes/user_routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	primary "github.com/Erodot0/gym-memeber-management/internals/adapters/primary"
+	secondary "github.com/Erodot0/gym-memeber-management/internals/adapters/secondary"
+	"github.com/Erodot0/gym-memeber-management/internals/app/handlers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func RegisterUserRoutes(app *fiber.App) {
+	handler := handlers.UserHandlers{
+		Parser: &primary.ErrorHandler{},
+		Http:   &secondary.HttpServices{},
+	}
+	
+	app.Post("/api/v1/users", handler.CreateUser)
+}
