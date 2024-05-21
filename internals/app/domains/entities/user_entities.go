4cb4e804d96c6c99facdba8bd8094d2eaafb7c8e
Erodot0 <janmanpreet.singh@gmail.com>
2024-05-21 17:51:25 +0200

    set email as gorm index

--- internals/app/domains/entities/user_entities.go
+++ internals/app/domains/entities/user_entities.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Email    string `json:"email" gorm:"not null,unique"`
+	Email    string `json:"email" gorm:"not null,unique;index"`
 	Name     string `json:"name" gorm:"not null"`
 	Surname  string `json:"surname" gorm:"not null"`
 	Password string `json:"-" gorm:"not null"`
