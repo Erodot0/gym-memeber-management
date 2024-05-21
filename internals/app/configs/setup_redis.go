e26e15a4351a67d2967b2542d8580250c2161497
Erodot0 <janmanpreet.singh@gmail.com>
2024-05-21 21:17:39 +0200

    added redis configuration

---
+++ internals/app/configs/setup_redis.go
@@ -0,0 +1,29 @@
+package configs
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/go-redis/redis/v8"
+)
+
+func InitializeRedisClient() (*redis.Client, error) {
+	// Create a new Redis client with the specified options
+	client := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_HOST") + ":" + os.Getenv("REDIS_PORT"),
+		Password: os.Getenv("REDIS_PWD"), 
+		DB:       0,
+	})
+
+	// Ping the Redis server to check if the connection is successful
+	_, err := client.Ping(context.Background()).Result()
+	if err != nil {
+		// If there's an error, close the client and return the error
+		client.Close()
+		return nil, fmt.Errorf("failed to connect to Redis: %v", err)
+	}
+
+	// Return the initialized client and nil error if connection is successful
+	return client, nil
+}
