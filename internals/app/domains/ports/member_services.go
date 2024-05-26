4a1d5dd13c91eedfb323d174dc734efb8ce34a05
Erodot0 <janmanpreet.singh@gmail.com>
2024-05-26 22:02:25 +0200

    added member service: GetMemberById and DeleteMember

--- internals/app/domains/ports/member_services.go
+++ internals/app/domains/ports/member_services.go
@@ -20,4 +20,22 @@ type MemberServices interface {
 	//   - []entities.Member: a slice of Member entities representing all members.
 	//   - error: an error if the retrieval process encounters any issues.
 	GetAllMembers() ([]entities.Member, error)
+	// GetMemberById retrieves a member from the database by their ID.
+	// 		Note: only active subscriptions is returned for the member.
+	// Parameters:
+	//   - m: the member entity to be retrieved, it must have the ID field set to the desired member's ID.
+	//
+	// Return type:
+	//   - error: an error if the retrieval process encounters any issues.
+	//
+	GetMemberById(m *entities.Member) error
+	// DeleteMember deletes a member from the database.
+	//		Note: It deletes the member and its associated entities.
+	// Parameters:
+	//   - m: the member entity to be deleted.
+	//
+	// Return type:
+	//   - error: an error if the deletion process encounters any issues.
+	//
+	DeleteMember(m *entities.Member) error
 }
--- internals/app/domains/services/member_services.go
+++ internals/app/domains/services/member_services.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"fmt"
+
 	"github.com/Erodot0/gym-memeber-management/internals/app/domains/entities"
 	"gorm.io/gorm"
 )
@@ -34,4 +36,22 @@ func (m *MemberServices) GetAllMembers() ([]entities.Member, error) {
 		return nil, err
 	}
 	return members, nil
+}
+
+func (m *MemberServices) GetMemberById(member *entities.Member) error {
+	return m.DB.
+		Preload("Contacts").
+		Preload("Address").
+		Preload("Subscription", "is_active = true").
+		Where("id = ?", member.ID).
+		First(member).
+		Error
+}
+
+func (m *MemberServices) DeleteMember(member *entities.Member) error {
+	fmt.Println("member: ", member.Name)
+	return m.DB.
+		Select("Contacts", "Address", "Subscription").
+		Delete(member).
+		Error
 }
