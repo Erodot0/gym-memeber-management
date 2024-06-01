215866f41701977f792a0fe528d57a00a6237bd0
Erodot0 <janmanpreet.singh@gmail.com>
2024-06-01 17:45:01 +0200

    added getMember middleware

--- internals/routes/member_routes.go
+++ internals/routes/member_routes.go
@@ -23,7 +23,7 @@ func (r *Routes) RegisterMemberRoutes() {
 	memberHandler := handlers.MembersHandlers{
 		Parser:   &primary.ErrorHandler{},
 		Http:     &secondary.HttpServices{},
-		Services:   &memberServices,
+		Services: &memberServices,
 	}
 
 	userMiddleware := middlewares.UserMiddlewares{
@@ -39,9 +39,9 @@ func (r *Routes) RegisterMemberRoutes() {
 
 	r.App.Post("/api/v1/members", userMiddleware.AuthorizeUser, userMiddleware.OnlyOwner, memberHandler.CreateMember)
 	r.App.Get("/api/v1/members", userMiddleware.AuthorizeUser, memberHandler.GetMembers)
-	
+
 	r.App.Get("/api/v1/members/:id", userMiddleware.AuthorizeUser, memberMiddleware.GetMember, memberHandler.GetMemberById)
-	r.App.Put("/api/v1/members/:id", userMiddleware.AuthorizeUser, userMiddleware.OnlyOwner, memberHandler.UpdateMember)
+	r.App.Put("/api/v1/members/:id", userMiddleware.AuthorizeUser, userMiddleware.OnlyOwner, memberMiddleware.GetMember, memberHandler.UpdateMember)
 	r.App.Delete("/api/v1/members/:id", userMiddleware.AuthorizeUser, userMiddleware.OnlyOwner, memberHandler.DeleteMember)
 
 	//Subscription
