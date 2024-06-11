8dc0546debce3f07e64fa929f12d983a20d66fa4
Erodot0 <janmanpreet.singh@gmail.com>
2024-06-11 16:53:26 +0200

    created services and port for roles

---
+++ internals/app/domains/ports/roles_ports.go
@@ -0,0 +1,54 @@
+package ports
+
+import "github.com/Erodot0/gym-memeber-management/internals/app/domains/entities"
+
+type RolesPort interface {
+	
+	// CreateRole creates a new role in the system.
+	//
+	// Parameters:
+	// - role: A pointer to the Roles struct representing the new role to be created.
+	//
+	// Returns:
+	// - error: An error object if there was an issue creating the role, otherwise nil.
+	//
+	CreateRole(role *entities.Roles) error
+
+	// GetAllRoles retrieves all roles from the system.
+	//
+	// It returns a slice of entities.Roles and an error if any occurred.
+	//
+	GetAllRoles() ([]entities.Roles, error)
+
+	// GetRole retrieves a role from the system by its ID.
+	//
+	// Parameters:
+	// - id: The ID of the role to retrieve.
+	//
+	// Returns:
+	// - *entities.Roles: A pointer to the Roles struct representing the retrieved role, or nil if not found.
+	// - error: An error object if there was an issue retrieving the role, otherwise nil.
+	//
+	GetRole(id uint) (*entities.Roles, error)
+
+	// UpdateRole updates a role in the system by its ID.
+	//
+	// Parameters:
+	// - id: The ID of the role to be updated.
+	// - role: A pointer to the UpdateRoles struct representing the updated role.
+	//
+	// Returns:
+	// - error: An error object if there was an issue updating the role, otherwise nil.
+	//
+	UpdateRole(id uint, role *entities.UpdateRoles) error
+
+	// DeleteRole deletes a role from the system by its ID.
+	//
+	// Parameters:
+	// - id: The ID of the role to be deleted.
+	//
+	// Returns:
+	// - error: An error object if there was an issue deleting the role, otherwise nil.
+	//
+	DeleteRole(id uint) error
+}
---
+++ internals/app/domains/services/roles_services.go
@@ -0,0 +1,42 @@
+package services
+
+import (
+	"github.com/Erodot0/gym-memeber-management/internals/app/domains/entities"
+	"gorm.io/gorm"
+)
+
+type RolesServices struct {
+	DB *gorm.DB
+}
+
+func (r *RolesServices) CreateRole(role *entities.Roles) error {
+	return r.DB.
+		Create(role).
+		Error
+}
+
+func (r *RolesServices) GetAllRoles() ([]entities.Roles, error) {
+	var roles []entities.Roles
+	if err := r.DB.
+		Preload("Users").
+		Find(&roles).Error; err != nil {
+		return nil, err
+	}
+	return roles, nil
+}
+
+func (r *RolesServices) GetRole(id uint) (*entities.Roles, error) {
+	var role entities.Roles
+	if err := r.DB.First(&role, id).Error; err != nil {
+		return nil, err
+	}
+	return &role, nil
+}
+
+func (r *RolesServices) UpdateRole(id uint, role *entities.UpdateRoles) error {
+	return r.DB.Model(&entities.Roles{}).Where("id = ?", id).Updates(role).Error
+}
+
+func (r *RolesServices) DeleteRole(id uint) error {
+	return r.DB.Delete(&entities.Roles{}, id).Error
+}
