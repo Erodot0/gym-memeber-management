ca5a8b82b616fc9a532532dbdbdb252c68926132
Erodot0 <janmanpreet.singh@gmail.com>
2024-06-12 22:23:51 +0200

    separeted permission structure

---
+++ internals/app/domains/entities/permissions_entities.go
@@ -0,0 +1,26 @@
+package entities
+
+import (
+	"gorm.io/gorm"
+)
+
+// Notes:
+//   - 0 -> no access
+//   - 1 -> access
+//   - 2 -> self access (only for it self)
+type Permissions struct {
+	gorm.Model
+	Table  string `json:"table" gorm:"not null;index"`
+	RoleId uint   `json:"role_id" gorm:"not null;index"`
+	Create uint   `json:"create"`
+	Read   uint   `json:"read"`
+	Update uint   `json:"update"`
+	Delete uint   `json:"delete"`
+}
+
+type UpdatePermissions struct {
+	Create uint `json:"create"`
+	Read   uint `json:"read"`
+	Update uint `json:"update"`
+	Delete uint `json:"delete"`
+}
--- internals/app/domains/entities/roles_enteties.go
+++ internals/app/domains/entities/roles_enteties.go
@@ -17,27 +17,6 @@ type UpdateRoles struct {
 	Name string `json:"name" gorm:"unique;not null;index"`
 }
 
-// Notes:
-//   - 0 -> no access
-//   - 1 -> access
-//   - 2 -> self access (only for it self)
-type Permissions struct {
-	gorm.Model
-	Table  string `json:"table" gorm:"not null;index"`
-	RoleId uint   `json:"role_id" gorm:"not null;index"`
-	Create uint   `json:"create"`
-	Read   uint   `json:"read"`
-	Update uint   `json:"update"`
-	Delete uint   `json:"delete"`
-}
-
-type UpdatePermissions struct {
-	Create uint `json:"create"`
-	Read   uint `json:"read"`
-	Update uint `json:"update"`
-	Delete uint `json:"delete"`
-}
-
 func (r *Roles) Validate() error {
 	if r.Name == "" {
 		return fmt.Errorf("inserire un nome")
@@ -49,22 +28,6 @@ func (r *Roles) Validate() error {
 func (r *UpdateRoles) Validate() error {
 	if r.Name == "" {
 		return fmt.Errorf("inserire un nome")
-	}
-
-	return nil
-}
-
-func (r *Permissions) Validate() error {
-	if r.Table == "" {
-		return fmt.Errorf("inserire una tabella")
-	}
-
-	if r.RoleId == 0 {
-		return fmt.Errorf("inserire un ruolo")
-	}
-
-	if r.Create > 1 || r.Update > 2 || r.Read > 2 || r.Delete > 2 {
-		return fmt.Errorf("inserire un permesso valido")
 	}
 
 	return nil
