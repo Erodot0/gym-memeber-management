4c4bd189916d274bdcbb6658d3088ab8958b282f
Erodot0 <janmanpreet.singh@gmail.com>
2024-06-13 14:50:56 +0200

    switch from table to table_name for permission column

--- internals/app/domains/entities/permissions_entities.go
+++ internals/app/domains/entities/permissions_entities.go
@@ -10,12 +10,12 @@ import (
 //   - 2 -> self access (only for it self)
 type Permissions struct {
 	gorm.Model
-	Table  string `json:"table" gorm:"not null;index"`
-	RoleId uint   `json:"role_id" gorm:"not null;index"`
-	Create uint   `json:"create"`
-	Read   uint   `json:"read"`
-	Update uint   `json:"update"`
-	Delete uint   `json:"delete"`
+	TableName string `json:"table_name" gorm:"not null;index"`
+	RoleId    uint   `json:"role_id" gorm:"not null;index"`
+	Create    uint   `json:"create"`
+	Read      uint   `json:"read"`
+	Update    uint   `json:"update"`
+	Delete    uint   `json:"delete"`
 }
 
 type UpdatePermissions struct {
