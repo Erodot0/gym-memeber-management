45b86e0acdf449f9a2ed7ab49a4003a508b08aaf
Erodot0 <janmanpreet.singh@gmail.com>
2024-06-15 21:57:40 +0200

    added pointer to uint and default value 0

--- internals/app/domains/entities/permissions_entities.go
+++ internals/app/domains/entities/permissions_entities.go
@@ -12,15 +12,15 @@ type Permissions struct {
 	gorm.Model
 	TableName string `json:"table_name" gorm:"not null;index"`
 	RoleId    uint   `json:"role_id" gorm:"not null;index"`
-	Create    uint   `json:"create"`
-	Read      uint   `json:"read"`
-	Update    uint   `json:"update"`
-	Delete    uint   `json:"delete"`
+	Create    *uint  `json:"create" gorm:"default:0"`
+	Read      *uint  `json:"read" gorm:"default:0"`
+	Update    *uint  `json:"update" gorm:"default:0"`
+	Delete    *uint  `json:"delete" gorm:"default:0"`
 }
 
 type UpdatePermissions struct {
-	Create uint `json:"create"`
-	Read   uint `json:"read"`
-	Update uint `json:"update"`
-	Delete uint `json:"delete"`
+	Create *uint `json:"create" gorm:"default:0"`
+	Read   *uint `json:"read" gorm:"default:0"`
+	Update *uint `json:"update" gorm:"default:0"`
+	Delete *uint `json:"delete" gorm:"default:0"`
 }
