518df1227f495ee14a6552fe35368f1a64c32217
Erodot0 <janmanpreet.singh@gmail.com>
2024-06-16 18:32:22 +0200

    formatting and created new middleware to check permissions

--- internals/app/tools/middlewares/user_middelwares.go
+++ internals/app/tools/middlewares/user_middelwares.go
@@ -2,6 +2,7 @@ package middlewares
 
 import (
 	"log"
+	"slices"
 
 	"github.com/Erodot0/gym-memeber-management/internals/app/domains/entities"
 	"github.com/Erodot0/gym-memeber-management/internals/app/domains/ports"
@@ -10,28 +11,37 @@ import (
 )
 
 type UserMiddlewares struct {
-	UserService ports.UserServices
-	Http        ports.HttpAdapters
+	http                ports.HttpAdapters
+	userService         ports.UserServices
+	permissionsServices ports.PermissionsServices
+}
+
+func NewUserMiddlewares(http ports.HttpAdapters, userService ports.UserServices, permissionsServices ports.PermissionsServices) *UserMiddlewares {
+	return &UserMiddlewares{
+		http:                http,
+		userService:         userService,
+		permissionsServices: permissionsServices,
+	}
 }
 
 func (m *UserMiddlewares) AuthorizeUser(c *fiber.Ctx) error {
 	authorization := c.Cookies("Authorization")
 	if authorization == "" {
 		// Send Unauthorized response
-		return m.Http.Unauthorized(c)
+		return m.http.Unauthorized(c)
 	}
 
 	// Get session from Redis
-	session, err := m.UserService.GetSessionByToken(authorization)
+	session, err := m.userService.GetSessionByToken(authorization)
 	if err != nil {
 		log.Printf("@AuthorizeUser: Error getting session: %v", err)
-		return m.Http.Unauthorized(c)
+		return m.http.Unauthorized(c)
 	}
 
 	// Check if it is the same IP and user agent
 	if session.IPAddress != c.IP() || session.UserAgent != c.Get("User-Agent") {
 		log.Printf("@AuthorizeUser: Session IP/User-Agent mismatch: %v", err)
-		return m.Http.Unauthorized(c)
+		return m.http.Unauthorized(c)
 	}
 
 	// Create user
@@ -39,24 +49,57 @@ func (m *UserMiddlewares) AuthorizeUser(c *fiber.Ctx) error {
 	user.ID = session.UserID
 
 	// Get user from database
-	if err := m.UserService.GetUserById(user); err != nil {
+	if err := m.userService.GetUserById(user); err != nil {
 		log.Printf("@AuthorizeUser: Error getting user: %v", err)
-		return m.Http.Unauthorized(c)
+		return m.http.Unauthorized(c)
 	}
 
 	// Set session
 	utils.SetLocals(c, "session", session)
 	utils.SetLocals(c, "user", user)
+	utils.SetLocals(c, "role", user.Role)
 	return c.Next()
 }
 
-// OnlyOwner is a middleware function that checks if the user is an owner.
-// If the user is not an owner, it returns a 403 error.
-func (m *UserMiddlewares) OnlyOwner(c *fiber.Ctx) error {
-	role := utils.GetLocalUser(c).Role
-	if role != "owner" {
-		return m.Http.Forbidden(c)
+func (m *UserMiddlewares) CheckPermissions(c *fiber.Ctx) error {
+	// get role
+	role := utils.GetLocalRole(c)
+	roleId := role.ID
+
+	if roleId == 0 {
+		return m.http.Forbidden(c)
 	}
 
+	// get requested action
+	requestedAction, requestedTable := m.permissionsServices.GetRequestedActionAndTable(c)
+	if requestedAction == "" || requestedTable == "" {
+		return m.http.Forbidden(c)
+	}
+
+	// get tables
+	tables, err := m.permissionsServices.GetTableList()
+	if err != nil {
+		log.Printf("@CheckPermissions: Error getting table list: %v", err)
+		return m.http.Forbidden(c)
+	}
+
+	// Check if the requested table is in the list of tables
+	if !slices.Contains(tables, requestedTable) {
+		return m.http.Forbidden(c)
+	}
+
+	permission, err := m.permissionsServices.HasPermission(requestedTable, roleId, requestedAction)
+	if err != nil {
+		log.Printf("@CheckPermissions: Error getting permission: %v", err)
+		return m.http.Forbidden(c)
+	}
+
+	if permission == 0 {
+		log.Printf("@CheckPermissions: User doesn't have permission to access this resource: %v, %v", requestedTable, requestedAction)
+		return m.http.Forbidden(c)
+	}
+
+	// Set permission
+	utils.SetLocals(c, "permission", permission)
 	return c.Next()
 }
