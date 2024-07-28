584ccbcad2c4dd523e22644155883c0dbfc8ec3c
Erodot0 <janmanpreet.singh@gmail.com>
2024-07-28 17:32:42 +0200

    fixed logs file not found on init

--- internals/app/configs/setup_logs.go
+++ internals/app/configs/setup_logs.go
@@ -3,13 +3,24 @@ package configs
 import (
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func InitLogs() {
-	logFile, err := os.OpenFile("./logs/app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	// Define the log directory and file
+	logDir := "./logs"
+	logFile := filepath.Join(logDir, "app.log")
+
+	// Create the log directory if it does not exist
+	if err := os.MkdirAll(logDir, os.ModePerm); err != nil {
+		log.Fatalf("failed to create log directory: %v", err)
+	}
+
+
+	file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatalf("failed to open log file: %v", err)
 	}
-	log.SetOutput(logFile)
+	log.SetOutput(file)
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 }
